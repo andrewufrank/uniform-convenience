b80eddc7c821fa38cf574c54c14c43c59fa349d7
Andrew Frank <frank@geoinfo.tuwien.ac.at>
2017-05-30 22:37:33 +0200

    changed readNote to readDef zero

--- Uniform/TestHarness.hs
+++ Uniform/TestHarness.hs
@@ -41,13 +41,14 @@ import           Test.Framework
 import Uniform.FileIO
 import Uniform.Error
 
-testDataDir = makeAbsDir  "/home/frank/Workspace8/LitTextWorkGeras/LitTextWork/TestData"
+--initializeTestDataDir :: ErrIO (Path Abs Dir)
+--initializeTestDataDir =   getAppUserDataDir "LitTextTest"
 
 
-testVar2File :: (Eq b, Show b, Read b) => a -> FilePath -> (a-> ErrIO b) -> IO ()
+testVar2File :: (Zeros b, Eq b, Show b, Read b) => a -> FilePath -> (a-> ErrIO b) -> IO ()
 -- ^ a text harness for the case that the start is a value (not a file)
 testVar2File  a resfile op = do
---    putIOwords ["read text for ", s2t . show $  textstate0]
+    putIOwords ["testVar2File read text for ", s2t resfile]
     t1 <- runErr $  op a
     case t1 of
         Left msg -> do
@@ -57,12 +58,14 @@ testVar2File  a resfile op = do
 --                    putIOwords ["the text result (for next) \n", showT tt1]
 --                    putIOwords ["the text result   \n",   tt1]
 --                    assertEqual result1B tt1
+                testDataDir <- getAppUserDataDir "LitTextTest"
                 let fn = testDataDir </> (resfile) :: Path Abs File
                 let fnx = testDataDir </> ("x" ++ resfile  ) :: Path Abs File
+                putIOwords ["test testVar2File", s2t resfile, showT fn]
                 fnexist <- doesFileExist fn
                 f1 <- if fnexist then readFile  (toFilePath fn)
                             else return zero
-                let testres = (readNote zero f1) == tt1
+                let testres = (readDef zero f1) == tt1
                 unless testres $
                     writeFile (toFilePath fnx )  (show tt1)
                 assertBool testres
@@ -72,7 +75,9 @@ testFile2File :: (Read a, Eq b, Show b, Read b, Zeros b) => FilePath -> FilePath
 -- test of purecode
 testFile2File  startfile resfile op = do
 --    putIOwords ["read text for ", s2t . show $  textstate0]
+    testDataDir <- getAppUserDataDir "LitTextTest"
     let fn0 =  testDataDir   </> startfile :: Path Abs File
+    putIOwords ["test testFile2File", s2t resfile, showT fn0]
     f0 <- readFile (toFilePath fn0)
 
     let tt1 =  op (readNote startfile f0)
@@ -93,7 +98,9 @@ testVar3File :: (Read a, Eq b, Show b, Read b, Zeros b) =>
 -- test of purecode
 testVar3File  base startfile resfile op = do
 --    putIOwords ["read text for ", s2t . show $  textstate0]
+    testDataDir <- getAppUserDataDir "LitTextTest"
     let fn0 =  testDataDir   </> startfile :: Path Abs File
+    putIOwords ["test testVar3File", s2t resfile, showT fn0]
     f0 <- readFile (toFilePath fn0)
 
     let tt1 =  op base (readNote startfile f0)
@@ -114,13 +121,15 @@ testVar3FileIO :: (Read a, Eq b, Show b, Read b, Zeros b) =>
 -- test of purecode
 testVar3FileIO  base startfile resfile op = do
 --    putIOwords ["read text for ", s2t . show $  textstate0]
+    testDataDir <- getAppUserDataDir "LitTextTest"
     let fn0 =  testDataDir   </> startfile :: Path Abs File
+    putIOwords ["test testVar3FileIO", s2t resfile, showT fn0]
     f0 <- readFile (toFilePath fn0)
 
     t1 <-  runErr $ op base (readNote startfile f0)
     case t1 of
         Left msg -> do
-                    putIOwords ["test testVar3FileIO", s2t resfile]
+                    putIOwords ["test testVar3FileIO", s2t resfile, showT f0]
                     assertBool False
         Right tt1 -> do
                 let fn = testDataDir </> (resfile) :: Path Abs File

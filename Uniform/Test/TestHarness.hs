c87d44ccfdc7e6f4cebd135b65cc1d1843f078ea
Andrew Frank <frank@geoinfo.tuwien.ac.at>
2018-03-28 20:44:52 +0200

    systematic renaming and rewriting of all testharness

--- Uniform/Test/TestHarness.hs
+++ Uniform/Test/TestHarness.hs
@@ -2,8 +2,11 @@
 --
 -- Module      :  Uniform.TestHarness
 --
--- | two functions to deal wtih tests which
+-- | functions to deal wtih tests which
 -- store data on disk
+-- naming test{Var}NFile{IO}
+-- N gives the number of input files 0..2
+-- IO is present when the function is -> ErrIO x
 
 -- attention: the test result throws an exception HUnit.NN (caused by assertBool)
 
@@ -45,24 +48,190 @@ import Uniform.Test.Utils
 
 testvardebug = True -- False
 
-testVar1File :: (Zeros b, Eq b, Show b, Read b, ShowTestHarness b)
+-- cases with no IO
+testVar0File :: (Zeros b, Eq b, Show b, Read b, ShowTestHarness b)
+            => Text -> a -> FilePath -> (a->  b) -> IO ()
+testVar0File progName  a resfile op = do
+        when testvardebug $ putIOwords ["testVar0File read text "]
+        r <- runErr $  sub progName  a resfile op
+        assertEqual (Right True) r
+    where
+        sub progName a resfile op =    do
+            testDataDir <- getLitTextTestDir3 progName
+            let t1 = op a
+            checkResult testvardebug testDataDir resfile t1
+
+testVar1File :: (Zeros b, Eq b, Show b, Read b, ShowTestHarness b
+                , Zeros c, Eq c, Show c, Read c, ShowTestHarness c)
+            => Text -> a -> FilePath -> FilePath -> (a->  b -> c ) -> IO ()
+testVar1File progName  a startfile resfile op = do
+        when testvardebug $ putIOwords ["testVar0File read text "]
+        r <- runErr $  sub progName  a startfile resfile op
+        assertEqual (Right True) r
+    where
+        sub progName a startfile resfile op =    do
+            testDataDir <- getLitTextTestDir3 progName
+            f0 <- readStartFile3 testvardebug testDataDir startfile
+            let t1 = op a (readTestH2 startfile f0)
+            checkResult testvardebug testDataDir resfile t1
+
+
+testVar2File :: (Zeros b, Eq b, Show b, Read b, ShowTestHarness b
+                , Zeros c, Eq c, Show c, Read c, ShowTestHarness c
+                , Zeros d, Read d, Show d, Eq d, ShowTestHarness d)
+            => Text -> a -> FilePath -> FilePath -> FilePath
+                    -> (a->  b -> c -> d ) -> IO ()
+testVar2File progName  a startfile secfile resfile op = do
+        when testvardebug $ putIOwords ["testVar0File read text "]
+        r <- runErr $  sub progName  a startfile secfile resfile op
+        assertEqual (Right True) r
+    where
+        sub progName a startfile secfile resfile op =    do
+            testDataDir <- getLitTextTestDir3 progName
+            f0 <- readStartFile3 testvardebug testDataDir startfile
+            f2 <- readStartFile3 testvardebug testDataDir secfile
+            let t1 = op a (readTestH2 startfile f0) (readTestH2 secfile f2)
+            checkResult testvardebug testDataDir resfile t1
+
+test1File :: (Zeros b, Eq b, Show b, Read b, ShowTestHarness b
+                , Zeros c, Eq c, Show c, Read c, ShowTestHarness c)
+            => Text ->  FilePath -> FilePath -> (b -> c ) -> IO ()
+test1File progName  startfile resfile op = do
+        when testvardebug $ putIOwords ["testVar0File read text "]
+        r <- runErr $  sub progName   startfile resfile op
+        assertEqual (Right True) r
+    where
+        sub progName  startfile resfile op =    do
+            testDataDir <- getLitTextTestDir3 progName
+            f0 <- readStartFile3 testvardebug testDataDir startfile
+            let t1 = op  (readTestH2 startfile f0)
+            checkResult testvardebug testDataDir resfile t1
+
+
+test2File :: (Zeros b, Eq b, Show b, Read b, ShowTestHarness b
+                , Zeros c, Eq c, Show c, Read c, ShowTestHarness c
+                , Zeros d, Read d, Show d, Eq d, ShowTestHarness d)
+            => Text ->  FilePath -> FilePath -> FilePath
+                    -> ( b -> c -> d ) -> IO ()
+test2File progName  startfile secfile resfile op = do
+        when testvardebug $ putIOwords ["testVar0File read text "]
+        r <- runErr $  sub progName  startfile secfile resfile op
+        assertEqual (Right True) r
+    where
+        sub progName  startfile secfile resfile op =    do
+            testDataDir <- getLitTextTestDir3 progName
+            f0 <- readStartFile3 testvardebug testDataDir startfile
+            f2 <- readStartFile3 testvardebug testDataDir secfile
+            let t1 = op  (readTestH2 startfile f0) (readTestH2 secfile f2)
+            checkResult testvardebug testDataDir resfile t1
+
+------ cases with IO
+
+testVar0FileIO :: (Zeros b, Eq b, Show b, Read b, ShowTestHarness b)
+            => Text -> a -> FilePath -> (a-> ErrIO b) -> IO ()
+testVar0FileIO progName  a resfile op = do
+        when testvardebug $ putIOwords ["testVar0FileIO read text "]
+        r <- runErr $  sub progName  a resfile op
+        assertEqual (Right True) r
+    where
+        sub progName a resfile op =    do
+            testDataDir <- getLitTextTestDir3 progName
+            t1 <- op a
+            checkResult testvardebug testDataDir resfile t1
+
+testVar1FileIO :: (Zeros b, Eq b, Show b, Read b, ShowTestHarness b
+                , Zeros c, Eq c, Show c, Read c, ShowTestHarness c)
+            => Text -> a -> FilePath -> FilePath -> (a->  b -> ErrIO c) -> IO ()
+testVar1FileIO progName  a startfile resfile op = do
+        when testvardebug $ putIOwords ["testVar0FileIO read text "]
+        r <- runErr $  sub progName  a startfile resfile op
+        assertEqual (Right True) r
+    where
+        sub progName a startfile resfile op =    do
+            testDataDir <- getLitTextTestDir3 progName
+            f0 <- readStartFile3 testvardebug testDataDir startfile
+            t1 <- op a (readTestH2 startfile f0)
+            checkResult testvardebug testDataDir resfile t1
+
+
+testVar2FileIO :: (Zeros b, Eq b, Show b, Read b, ShowTestHarness b
+                , Zeros c, Eq c, Show c, Read c, ShowTestHarness c
+                , Zeros d, Read d, Show d, Eq d, ShowTestHarness d)
+            => Text -> a -> FilePath -> FilePath -> FilePath
+                    -> (a->  b -> c -> ErrIO d) -> IO ()
+testVar2FileIO progName  a startfile secfile resfile op = do
+        when testvardebug $ putIOwords ["testVar0FileIO read text "]
+        r <- runErr $  sub progName  a startfile secfile resfile op
+        assertEqual (Right True) r
+    where
+        sub progName a startfile secfile resfile op =    do
+            testDataDir <- getLitTextTestDir3 progName
+            f0 <- readStartFile3 testvardebug testDataDir startfile
+            f2 <- readStartFile3 testvardebug testDataDir secfile
+            t1 <- op a (readTestH2 startfile f0) (readTestH2 secfile f2)
+            checkResult testvardebug testDataDir resfile t1
+
+test1FileIO :: (Zeros b, Eq b, Show b, Read b, ShowTestHarness b
+                , Zeros c, Eq c, Show c, Read c, ShowTestHarness c)
+            => Text ->  FilePath -> FilePath -> (b -> ErrIO c) -> IO ()
+test1FileIO progName  startfile resfile op = do
+        when testvardebug $ putIOwords ["testVar0FileIO read text "]
+        r <- runErr $  sub progName   startfile resfile op
+        assertEqual (Right True) r
+    where
+        sub progName  startfile resfile op =    do
+            testDataDir <- getLitTextTestDir3 progName
+            f0 <- readStartFile3 testvardebug testDataDir startfile
+            t1 <- op  (readTestH2 startfile f0)
+            checkResult testvardebug testDataDir resfile t1
+
+
+test2FileIO :: (Zeros b, Eq b, Show b, Read b, ShowTestHarness b
+                , Zeros c, Eq c, Show c, Read c, ShowTestHarness c
+                , Zeros d, Read d, Show d, Eq d, ShowTestHarness d)
+            => Text ->  FilePath -> FilePath -> FilePath
+                    -> ( b -> c -> ErrIO d) -> IO ()
+test2FileIO progName  startfile secfile resfile op = do
+        when testvardebug $ putIOwords ["testVar0FileIO read text "]
+        r <- runErr $  sub progName  startfile secfile resfile op
+        assertEqual (Right True) r
+    where
+        sub progName  startfile secfile resfile op =    do
+            testDataDir <- getLitTextTestDir3 progName
+            f0 <- readStartFile3 testvardebug testDataDir startfile
+            f2 <- readStartFile3 testvardebug testDataDir secfile
+            t1 <- op  (readTestH2 startfile f0) (readTestH2 secfile f2)
+            checkResult testvardebug testDataDir resfile t1
+
+{-
+testVar0FileIO :: (Zeros b, Eq b, Show b, Read b, ShowTestHarness b)
             => Text -> a -> FilePath -> (a-> ErrIO b) -> IO ()
 -- ^ a text harness for the case that the start is a value (not a file)
 -- the progname gives the hidden directory of the test results (i.e. ~/.progName)
 testVar1File progName  a resfile op = do
-    when testvardebug $ putIOwords ["testVar1File read text "
+    when testvardebug $ putIOwords ["testVar0FileIO read text "
                 , "with expected result ", showT resfile]
-    r <- runErr $  testVar1File' progName  a resfile op
+    r <- runErr $  testVar0FileIO' progName  a resfile op
     assertEqual (Right True) r
+  where
+    testVar0FileIO' progName  a resfile op = do
+        t1 <- op a
+        testDataDir <- getLitTextTestDir3 progName
+        checkResult testvardebug testDataDir resfile t1
 
-testVar1File' :: (Zeros b, Eq b, Show b, Read b, ShowTestHarness b)
-            => Text -> a -> FilePath -> (a-> ErrIO b) -> ErrIO Bool
+testVar0File :: (Zeros b, Eq b, Show b, Read b, ShowTestHarness b)
+            => Text -> a -> FilePath -> (a->  b) -> IO ()
+testVar0File progName  a resfile op = do
+        when testvardebug $ putIOwords ["testVar0File read text "]
+        let t1 = op a
+        testDataDir <- getLitTextTestDir3 progName
+        r <- checkResult testvardebug testDataDir resfile t1
+        assertBool r
+
+testVar2File' :: (Zeros b, Eq b, Show b, Read b, ShowTestHarness b)
+            => Text -> a -> FilePath -> FilePath -> (a-> b -> ErrIO b) -> ErrIO Bool
 -- ^ a text harness for the case that the start is a value (not a file)
 -- the progname gives the hidden directory of the test results (i.e. ~/.progName)
-testVar1File' progName  a resfile op = do
-    t1 <- op a
-    testDataDir <- getLitTextTestDir3 progName
-    checkResult testvardebug testDataDir resfile t1
 
 
 testFile2File :: (Read a, Eq b, Show b, Read b, Zeros b
@@ -196,5 +365,5 @@ testVar2FileIO' progName base startfile resfile op = do
     t1 <- op base (readTestH2 "testVar2FileIO" f0)
     checkResult testvardebug  testDataDir resfile t1
 
-
+-}
 
